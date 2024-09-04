7328c4f687eb96ae326a4c25eedf6ee2b4417980
khanloo <m.nadirkhanloo1380@gmail.com>
2024-09-04 20:04:16 +0330

    Fix : Fetch

--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
-export default function useFetch(search = "", url) {
+export default function useFetch(url, query = "") {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -10,7 +10,7 @@ export default function useFetch(search = "", url) {
     async function fetchData() {
       try {
         setIsLoading(true);
-        const { data } = await axios.get(`${url}?${search}`);
+        const { data } = await axios.get(`${url}?${query}`);
         setData(data);
       } catch (err) {
         setData([]);
@@ -19,8 +19,9 @@ export default function useFetch(search = "", url) {
         setIsLoading(false);
       }
     }
-    fetchData();
-  }, [search, url]);
 
-  return { data, isLoading };
+    fetchData();
+  }, [query, url]);
+
+  return { isLoading, data };
 }
