648b5805fe9ac0e5f207c9d6e0bb3a89cc48e387
khanloo <m.nadirkhanloo1380@gmail.com>
2024-09-04 20:03:38 +0330

    Feat : add list item

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from "@mui/material/styles";
 import Header from "./components/Header";
 import { Theme } from "./components/ui/Theme";
+import { Toaster } from "react-hot-toast";
+import ListItem from "./components/ListItem";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={Theme}>
+        <Toaster />
         <Header />
-
+        <ListItem />
       </ThemeProvider>
     </>
   );
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,41 @@
+import { Container } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid2";
+import Typography from "@mui/material/Typography";
+import useFetch from "../hook/useFetch";
+
+function ListItem() {
+  const { data, isLoading } = useFetch(" http://localhost:5000", "");
+  if (isLoading) <p>loading</p>;
+  return (
+    <Container>
+      <Grid container spacing={5} sx={{ mt: 5 }}>
+        {data.map((item) => {
+          <Grid size={4}>
+            <Card key={item.id} sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt={item.name}
+                height="140"
+                image={item.picture_url.url}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.smart_location}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {item.name}
+                </Typography>
+                <Typography variant="body1">{item.price}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>;
+        })}
+      </Grid>
+    </Container>
+  );
+}
+
+export default ListItem;
