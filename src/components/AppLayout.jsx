a2c00838f1818cf8ce693df6e48e2f080eb706ff
khanloo <m.nadirkhanloo1380@gmail.com>
2024-09-05 19:56:19 +0330

    Feat : add app layout

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,8 +1,20 @@
+import { Container } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import { Outlet } from "react-router-dom";
 
 function AppLayout() {
   return (
-    <div>AppLayout</div>
-  )
+    <Container>
+      <Grid container>
+        <Grid size={5}>
+          <Outlet />
+        </Grid>
+        <Grid size={7}>
+          <p>map</p>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
