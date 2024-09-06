9c3951cb4c7576f186f328421f2f5582426e82ef
khanloo <m.nadirkhanloo1380@gmail.com>
2024-09-06 17:35:54 +0330

    fix: context item

--- src/components/context/ListFilterProvider.jsx
+++ src/components/context/ListFilterProvider.jsx
@@ -7,21 +7,22 @@ const ListFilterContext = createContext();
 export default function ListFilterProvider({ children }) {
   const [searchParams, setSearchParams] = useSearchParams();
   const valueSearch = searchParams.get("value");
-  const filterNumber = JSON.parse(searchParams.get("options"));
+  const filterNumber = JSON.parse(searchParams.get("options"))?.room;
 
-  const { data, isLoading } = useFetch(
+  const { data: hotels, isLoading } = useFetch(
     "http://localhost:5000/hotels",
     `host_location_like=${valueSearch || ""}&accommodates_gte=${
-      filterNumber.room || 1
+      filterNumber || 1
     }`
   );
   return (
-    <ListFilterContext.Provider value={{ data, isLoading }}>
+    <ListFilterContext.Provider value={{ hotels, isLoading }}>
       {children}
     </ListFilterContext.Provider>
   );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useFilterHotel() {
   return useContext(ListFilterContext);
 }
