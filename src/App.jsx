614969252c87b430c6d385c4ba570580dac42146
khanloo <m.nadirkhanloo1380@gmail.com>
2024-09-07 17:45:07 +0330

    Feat : add page bookmark

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from "@mui/material/styles";
-import Header from "./components/Header";
-import { Theme } from "./components/ui/Theme";
 import { Toaster } from "react-hot-toast";
-import ListItem from "./components/ListItem";
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
-import ListFilter from "./components/ListFilter";
+import BookmarkItem from "./components/BookmarkItem";
 import ListFilterProvider from "./components/context/ListFilterProvider";
+import Header from "./components/Header";
 import Item from "./components/Item";
+import ListFilter from "./components/ListFilter";
+import ListItem from "./components/ListItem";
+import { Theme } from "./components/ui/Theme";
 
 function App() {
   return (
@@ -20,10 +21,11 @@ function App() {
               <Route index element={<ListItem />} />
               <Route path="filter" element={<AppLayout />}>
                 <Route index element={<ListFilter />} />
-                <Route
-                  path=":id"
-                  element={<Item />}
-                />
+                <Route path=":id" element={<Item />} />
+              </Route>
+              <Route path="bookmark" element={<BookmarkItem />}>
+                <Route index element={<div>bookmark list</div>} />
+                <Route path="add" element={<div>add bookmark</div>} />
               </Route>
             </Route>
           </Routes>
