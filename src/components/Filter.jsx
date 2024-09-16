03ad79e4945c3b31078a8db20b099487c4f51d69
khanloo <m.nadirkhanloo1380@gmail.com>
2024-09-16 18:40:35 +0330

    Feat : add component filter

---
+++ src/components/Filter.jsx
@@ -0,0 +1,70 @@
+import SavedSearchIcon from "@mui/icons-material/SavedSearch";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import * as React from "react";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import {
+  createSearchParams,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
+import Daterange from "./Date";
+import DialogSelect from "./ui/DialogSelect";
+import InputWithIcon from "./ui/InputWithIcon";
+
+function Filter() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [value, setValue] = useState(searchParams.get("value") || "");
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+      color: "green",
+    },
+  ]);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
+  const navigate = useNavigate();
+
+  const handelSearch = () => {
+    const encodeParams = createSearchParams({
+      date: JSON.stringify(date),
+      value,
+      options: JSON.stringify(options),
+    });
+
+    navigate({
+      pathname: "filter",
+      search: encodeParams.toString(),
+    });
+  };
+  return (
+    <Box>
+      <Grid container spacing={2} sx={{ alignItems: "center" }}>
+        <Grid item md={4} xs={12} sx={{ textAlign: "center" }}>
+          <InputWithIcon value={value} setValue={setValue} />
+        </Grid>
+        <Grid item md={4} xs={12} sx={{ textAlign: "center" }}>
+          <Daterange date={date} setDate={setDate} />
+        </Grid>
+        <Grid item md={3} xs={12} sx={{ textAlign: "center" }}>
+          <DialogSelect options={options} setOptions={setOptions} />
+        </Grid>
+        <Grid item md={1} xs={12} sx={{ textAlign: "center" }}>
+          <IconButton aria-label="delete" size="large" onClick={handelSearch}>
+            <SavedSearchIcon color="primary" fontSize="inherit" />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Filter;
